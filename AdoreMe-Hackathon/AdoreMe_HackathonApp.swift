4e773a9262e7156c268fe132a61ba3f049d5f039
Andrei Mirica <andrei.m@adoreme.com>
2023-07-06 12:31:24 +0300

    Initial Commit

---
+++ AdoreMe-Hackathon/AdoreMe_HackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdoreMe_HackathonApp.swift
+//  AdoreMe-Hackathon
+//
+//  Created by Andrei.M-AM on 06.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct AdoreMe_HackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
